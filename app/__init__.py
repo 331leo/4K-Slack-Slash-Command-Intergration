44e2cb5422dca2190436ea51c4d8254d5005940b
331leo <331leo@outlook.com>
2021-04-12 01:28:26 +0900

    feat: add more information on message after registeration

--- app/__init__.py
+++ app/__init__.py
@@ -42,12 +42,20 @@ async def slash_createmail(slash: slack.SlashCommand = Depends()):
             text = f"""
 성공적으로 이메일 발급에 성공하였습니다!
 아래 인증정보를 통해 웹 메일에 로그인 하실 수 있습니다.
-로그인 후, `설정->암호` 에서 비밀번호를 변경하실 수 있습니다.
+로그인 후, `설정->암호` 에서 비밀번호를 꼭 변경 후 사용하시기 바랍니다.
 
 https://{os.environ.get("mail_server_domain")}
 ```
 ID: {data.get("email")}
 PW: {data.get("password")}
+```
+
+다른 메일 클라이언트를 사용하시려면 아래 설정을 입력하세요.
+```
+받는 메일 서버(IMAP): {os.environ.get("mail_server_domain")} (기본포트 993)
+보내는 메일 서버(SMTP): {os.environ.get("mail_server_domain")} (기본포트 587)
+Username: 이메일 주소 전체
+Password: 비밀번호
 ```
             """
             db.set(slash.user_id,data.get("email"))
