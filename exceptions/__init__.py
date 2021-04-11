3272a1c940c6727f701b4d587bba8ff9ce0edf02
331leo <331leo@outlook.com>
2021-04-12 00:14:04 +0900

    feat: add custom exceptions

---
+++ exceptions/__init__.py
@@ -0,0 +1,23 @@
+class InvaildDomain(Exception):
+    def __init__(self, domain):
+        self.text = f"`@{domain}`은 사용할수 없습니다."
+    def __str__(self):
+        return self.text
+
+class InvaildInput(Exception):
+    def __init__(self, input):
+        self.text = f"`{input}` 은 올바르지 않은 입력입니다."
+    def __str__(self):
+        return self.text
+
+class AlreadyExsistingEmail(Exception):
+    def __init__(self, email):
+        self.text = f"`{email}`은 이미 사용중입니다. 다른 이메일 주소를 사용해 주세요."
+    def __str__(self):
+        return self.text
+
+class AlreadyExsistingUser(Exception):
+    def __init__(self, userid):
+        self.text = f"<@{userid}> 님은 이미 등록된 이메일 주소가 있습니다."
+    def __str__(self):
+        return self.text
