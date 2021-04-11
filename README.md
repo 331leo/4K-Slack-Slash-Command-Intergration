15f9a6104f7d4a4c8e3d41d78ff2d32a0ed10508
331leo <331leo@outlook.com>
2021-04-11 22:17:29 +0900

    refactor: move files to folder

---
+++ README.md
@@ -0,0 +1,2 @@
+# C4K-Slack-Slash-Command-Intergration
+> 코드포코리아 슬랙의 Slash Command를 위한 FastAPI 서버입니다.
--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,22 @@ from fastapi import FastAPI
 from fastapi.param_functions import Depends
 from dotenv import load_dotenv
 load_dotenv()
-import slack_util
+import slack
 
-app = FastAPI()
-app.include_router(slack_util.router)
+app = FastAPI(
+    title="C4K-Slack-Slash-Command-Intergration"
+)
+app.include_router(slack.router)
 
 def gen_slack_message(text: str):
     return {"blocks":[{"type":"section","text":{"type":"mrkdwn","text":text}}]}
 
 @app.get("/")
-def route_root():
+async def route_root():
     return {"message":"This is Slack Slash Command API for codefor.kr"}
 
 @app.post("/createmail")
-def slash_createmail(slash: slack_util.SlashCommand = Depends()):
+async def slash_createmail(slash: slack.SlashCommand = Depends()):
     print(slash)
     text = "hello world"
     return gen_slack_message(text)
